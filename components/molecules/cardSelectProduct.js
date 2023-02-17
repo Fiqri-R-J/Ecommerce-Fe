f6bfa6c4ecef951db316d7973a30e444f6734007
Maulana Ismail <maulanaismail3466@gmail.com>
2023-02-17 15:41:36 +0700

    [ADD] feature add total produk

--- components/molecules/cardSelectProduct.js
+++ components/molecules/cardSelectProduct.js
@@ -3,7 +3,16 @@ import React from "react";
 import style from "@/styles/pages/myBagStyle.module.scss";
 import Link from "next/link";
 
-export default function cardSelectProduct() {
+export default function cardSelectProduct(props) {
+  const {
+    img,
+    nameProduk,
+    brand,
+    total,
+    price,
+    handleOnIncrease,
+    handleOnDecrease,
+  } = props;
   return (
     <div>
       <div
@@ -14,15 +23,11 @@ export default function cardSelectProduct() {
           {/* PHOTO PRODUCT */}
           <div className="col-2">
             <div class="form-check d-flex align-items-center">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-              />
+              <input className="form-check-input" type="checkbox" value="" />
               <label className="form-check-label d-inline-block">
                 <img
                   className={`ms-3 ${style.icon}`}
-                  src="/images/product.webp"
+                  src={img}
                   alt="icon-navbar"
                 />
               </label>
@@ -30,16 +35,53 @@ export default function cardSelectProduct() {
           </div>
           {/* PRODUCT NAME */}
           <div className={`col-4 mt-1 ${style.productName}`}>
-            <h5>Mens formal suit - Black</h5>
-            <p>Zalora Cloth</p>
+            <h5>{nameProduk}</h5>
+            <p>{brand}</p>
           </div>
           {/* TOTAL */}
-          <div className={`col-3`}></div>
+          <div className={`col-3 text-end`}>
+            <nav aria-label="Page navigation example ">
+              <ul class="pagination">
+                <li class="page-item">
+                  <a
+                    class="page-link border rounded-circle border-2"
+                    aria-label="Previous"
+                    style={{ borderRadius: "50px", borderColor: "black" }}
+                    onClick={() => {
+                      handleOnDecrease();
+                    }}
+                  >
+                    <span aria-hidden="true" style={{ color: "black" }}>
+                      -
+                    </span>
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link border-0" style={{ color: "black" }}>
+                    {total}
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a
+                    class="page-link border rounded-circle border-2"
+                    aria-label="Next"
+                    onClick={() => {
+                      handleOnIncrease();
+                    }}
+                  >
+                    <span aria-hidden="true" style={{ color: "black" }}>
+                      +
+                    </span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
           {/* TOTAL PRICE */}
           <div
             className={`col-2 offset-1 d-flex align-items-center ${style.totalPrice}`}
           >
-            <h5 className="text-end">$ 20.0</h5>
+            <h5 className="text-end">{price}</h5>
           </div>
         </div>
       </div>
